7cf1029a0a2fbb4bede160ac0a957d4409123363
Joshua Ball <joshua.ball@ttu.edu>
2020-10-08 20:12:51 -0700

    Update Image_downsample.R

--- Image_downsample.R
+++ Image_downsample.R
@@ -9,13 +9,20 @@ library(magick)
 #Used to filter noise in images
 library(EBImage)
 library(wvtool)
+library(foreach)
+#For parallelization
+library(parallel)
+cl <- parallel::makePSOCKcluster(11)#Utilizing 11 of 12 cores on my computer
+doParallel::registerDoParallel(cl)
 #set working directory to your respective folder
 setwd("C:\\Users\\JoshPC\\Desktop\\food-101")
 
 
 #Iterating through the images
 food101Data <- list.files(path = directoryToImages,
-                    full.names = TRUE, recursive = TRUE)
+                          full.names = TRUE, recursive = TRUE)
+test <- list.files(path = "apple_pie//",
+                   full.names = TRUE, recursive = TRUE)
 #Moving our food101Data object into a dataframe
 data <- data.frame
 data <- food101Data
@@ -25,7 +32,8 @@ head(data,10)
 tail(data,10)
 
 #iterating through data frame of images
-for(x in 1:length(data)) {
+#This is now parallized to use all my CPU cores. In practice should only take 20% of time as before.
+foreach(x = 1:length(data), .packages=c("magick","imager","EBImage") ) %dopar% {
   #loading image in subdirectory 
   file <- image_read(data[x])
   #Printing for user to be able to follow progress 
@@ -41,4 +49,3 @@ for(x in 1:length(data)) {
   #Save image, overwriting old image
   save.image(img_median, file = gsub("JPG", "jpg", paste(data[x])))
 }
-
